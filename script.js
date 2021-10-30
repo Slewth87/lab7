2ea3b7861b638bd2bbaace2aa132c1e5db23338c
Richard Duffy <rduffy@slack-corp.com>
2021-10-30 19:04:02 +0100

    Week 7 Lab

---
+++ script.js
@@ -0,0 +1,9 @@
+function submission(){
+    var name = document.getElementById("userName").value;
+    var email = document.getElementById("email").value;
+    if (name != "" && email != ""){
+        document.getElementById("form").style.display = "none";
+        document.getElementById("output").innerHTML=name+", thank you for your details. We will be in touch via " + email + " shortly.";
+        document.getElementById("output").style.display = "block";
+    }
+}
