4a70b02c61324a7f14f7f167819f2a13d63f059d
Richard Duffy <rduffy@slack-corp.com>
2021-11-02 18:45:54 +0000

    Better event default handling

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ function submission(){
         document.getElementById("form").style.display = "none";
         document.getElementById("output").innerHTML=name+", thank you for your details. We will be in touch via " + email + " shortly.";
         document.getElementById("output").style.display = "block";
+        event.preventDefault();
     }
 }
